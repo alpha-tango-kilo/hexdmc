b5074a8c72ac243ab722022ccea4bed41b34664e
alpha-tango-kilo <git@heyatk.com>
2022-01-15 14:35:54 +0000

    Restructure a bit
    Better supports upcoming changes

--- src/main.rs
+++ src/main.rs
@@ -74,13 +74,7 @@ impl Colour {
     }
 
     fn format_hex(&self) -> String {
-        format!(
-            "#{:02x?}{:02x?}{:02x?} {}",
-            self.r,
-            self.g,
-            self.b,
-            FULL_BLOCK.color(self.owo())
-        )
+        hex_from_rgb([self.r, self.g, self.b])
     }
 
     fn diff(&self, other: Rgb) -> u16 {
@@ -108,25 +102,24 @@ fn main() -> Result<()> {
     let subcommand = env::args().nth(1).context("No subcommand provided")?;
 
     let processing_fn = match subcommand.to_ascii_lowercase().as_str() {
-        "hex" => process_hex_str,
-        "dmc" => process_dmc_str,
+        "hex" => match_hex_str,
+        "dmc" => match_dmc_str,
         _ => bail!("invalid subcommand"),
     };
 
     env::args().skip(2).try_for_each(processing_fn)
 }
 
-fn process_hex_str<S: AsRef<str>>(hex_str: S) -> Result<()> {
-    let hex_str = hex_str.as_ref();
-    let hex_str = hex_str.strip_prefix('#').unwrap_or(hex_str);
+fn match_hex_str<S: AsRef<str>>(hex_str: S) -> Result<()> {
     let rgb = rgb_from_hex(hex_str)?;
+    let hex_str = hex_from_rgb(rgb); // standardises format
     let colour: RgbMatch = COLOUR_MAP.lookup_rgb(rgb);
 
     use RgbMatch::*;
     match colour {
         Exact(c) => println!(
-            "#{} {} -> {}",
-            hex_str,
+            "{} {} -> {}",
+            &hex_str,
             FULL_BLOCK.color(rgb_owo(rgb)),
             c.format_dmc()
         ),
@@ -143,8 +136,8 @@ fn process_hex_str<S: AsRef<str>>(hex_str: S) -> Result<()> {
                 .intersperse(String::from(", or "))
                 .collect::<String>();
             println!(
-                "#{} {} ~> {}",
-                hex_str,
+                "{} {} ~> {}",
+                &hex_str,
                 FULL_BLOCK.color(rgb_owo(rgb)),
                 dmcs_string
             );
@@ -153,22 +146,34 @@ fn process_hex_str<S: AsRef<str>>(hex_str: S) -> Result<()> {
     Ok(())
 }
 
-fn process_dmc_str<S: AsRef<str>>(dmc_str: S) -> Result<()> {
+fn match_dmc_str<S: AsRef<str>>(dmc_str: S) -> Result<()> {
     let colour: &Colour = COLOUR_MAP.lookup_floss(dmc_str.as_ref())?;
     println!("{} -> {}", dmc_str.as_ref(), colour.format_hex());
     Ok(())
 }
 
 // e.g. "123455", "ab34ee", "AF1234"
-fn rgb_from_hex(s: &str) -> Result<Rgb> {
-    if s.len() == 6 {
-        let r = u8::from_str_radix(&s[..2], 16)?;
-        let g = u8::from_str_radix(&s[2..4], 16)?;
-        let b = u8::from_str_radix(&s[4..], 16)?;
+fn rgb_from_hex<S: AsRef<str>>(hex_str: S) -> Result<Rgb> {
+    let hex_str = hex_str.as_ref();
+    let hex_digits = hex_str.strip_prefix('#').unwrap_or(hex_str);
+    if hex_digits.len() == 6 {
+        let r = u8::from_str_radix(&hex_digits[..2], 16)?;
+        let g = u8::from_str_radix(&hex_digits[2..4], 16)?;
+        let b = u8::from_str_radix(&hex_digits[4..], 16)?;
         Ok([r, g, b])
     } else {
         bail!("not hex string")
     }
+}
+
+fn hex_from_rgb(rgb: Rgb) -> String {
+    format!(
+        "#{:02x}{:02x}{:02x} {}",
+        rgb[0],
+        rgb[1],
+        rgb[2],
+        FULL_BLOCK.color(rgb_owo(rgb))
+    )
 }
 
 fn rgb_owo(rgb: Rgb) -> DynColors {
